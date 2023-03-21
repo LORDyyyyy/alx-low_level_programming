d177ed2be951723071d003cb85b2218edf3c3589
LORDyyyyy <mlord6271@gmail.com>
2023-03-21 20:38:44 +0000

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	long f = 0, s = 1, i, t;
 
-	for (i = 1; i < 50; i++)
+	for (i = 1; i <= 50; i++)
 	{
 		t = f + s;
 		printf("%ld", t);
 		f = s;
 		s = t;
-		if (i != 49)
+		if (i != 50)
 			printf(", ");
 	}
 	return (0);
