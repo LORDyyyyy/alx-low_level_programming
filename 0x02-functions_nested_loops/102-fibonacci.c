50e76fca531b93d4fc4a4d60a7b56efc081eaf9a
LORDyyyyy <mlord6271@gmail.com>
2023-03-21 20:34:25 +0000

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void);
+{
+	unsigned long long f = 0, s = 1, i, t;
+
+	for (i = 1; i <= 50; i++)
+	{
+		t = f + s;
+		printf("%llu", t);
+		f = s;
+		s = t;
+		if (i != 50)
+			printf(", ");
+	}
+	return (0);
+}
