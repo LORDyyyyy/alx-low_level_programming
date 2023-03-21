25a7312681164a77607cb2c7d3a3d1fb318d5566
LORDyyyyy <mlord6271@gmail.com>
2023-03-21 18:40:30 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,10 @@
 void print_alphabet_x10(void)
 {
 	char i, n;
-	
+
 	for (n = '0'; n <= '9'; n++)
 	{
-		for (i = 'a'; i <='z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 		_putchar('\n');
 	}
