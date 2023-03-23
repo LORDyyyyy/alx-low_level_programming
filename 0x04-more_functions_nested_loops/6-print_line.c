9d14d674a840dedef669c4169ca4b6f72b26fe9f
LORDyyyyy <mlord6271@gmail.com>
2023-03-23 09:24:22 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_line - print _ 
+ * @n: number of spaces
+ * Return: void
+ */
+void print_line(int n)
+{
+	for (; n > 0; n--)
+		_putchar('-');
+	_putchar('\n');
+}
