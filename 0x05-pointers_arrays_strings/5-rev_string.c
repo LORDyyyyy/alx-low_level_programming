8fb7bc75d49685f2f33f9719d528631350afc3bf
LORDyyyyy <mlord6271@gmail.com>
2023-03-27 18:05:49 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,11 @@ void rev_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 		len++;
-	char s2[1000];
+	for (i = 0, j = len - 1; i < j; j--, i++)
+	{
+		char temp = s[i];
 
-	for (i = 0; i < len; i++)
-		s2[i] = s[i];
-	for (i = 0, j = len - 1; j >= 0; j--, i++)
-		s[j] = s2[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
 }
