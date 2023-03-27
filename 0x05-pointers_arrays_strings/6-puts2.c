d8ef8aef144bd3fbd2271d38412fee17d1bf204f
LORDyyyyy <mlord6271@gmail.com>
2023-03-27 18:08:18 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string
+ * @str: the string
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
