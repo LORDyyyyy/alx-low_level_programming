f74e3345b1ca8e6bcd6ae3462a09ab9967f4b016
LORDyyyyy <mlord6271@gmail.com>
2023-03-27 17:37:51 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order
+ * @s: the string
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+		putchar(s[i]);
+	putchar('\n');
+}
+
