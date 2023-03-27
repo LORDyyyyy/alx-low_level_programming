36bf3332a649adf9e5d1344245920a3a933ef34b
LORDyyyyy <mlord6271@gmail.com>
2023-03-27 18:20:50 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,8 @@ void puts2(char *str)
 {
 	int i, len = 0;
 
-	for (i = 0; str[i] != '0'; i++)
+	while (str[len] != '\0')
 		len++;
-
 	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
