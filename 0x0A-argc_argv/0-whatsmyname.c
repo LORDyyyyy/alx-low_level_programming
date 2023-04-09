57499d15084044f503388a060e5a388f61116c5e
LORDyyyyy <mlord6271@gmail.com>
2023-04-09 13:41:14 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argv: array
+ * @argc: argv size
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
