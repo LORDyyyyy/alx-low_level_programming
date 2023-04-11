6eac8cf560255a8b3303bfb6ebefbf66a561fadc
LORDyyyyy <mlord6271@gmail.com>
2023-04-11 23:34:12 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,10 +11,10 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return (NULL);
 	char *arr = malloc(size);
 
+	if (size == 0 || arr == 0)
+		return (NULL);
 	while (size--)
 		arr[size] = c;
 	return (arr);
