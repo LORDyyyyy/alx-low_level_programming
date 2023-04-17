0ede0a42165f0f260ebb447d8550f8a814200ac5
LORDyyyyy <mlord6271@gmail.com>
2023-04-17 11:43:52 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - a struct for a dog name/age/owner name
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Description: First task
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
