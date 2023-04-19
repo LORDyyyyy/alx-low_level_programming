eec70df723e9d9d580ae47ddffbd1b0f3b1c6c9e
LORDyyyyy <mlord6271@gmail.com>
2023-04-19 13:24:40 +0000

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argv: array
+ * @argc: argv size
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	char *p = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	i = atoi(argv[1]);
+
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	while (i--)
+		printf("%02hhx ", *p++);
+
+	printf("\n");
+	return (0);
+}
