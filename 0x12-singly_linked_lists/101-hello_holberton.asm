6fb1c2be38e9806ebfcae9ba97fff62e3e5de4ca
LORDyyyyy <mlord6271@gmail.com>
2023-04-27 17:34:37 +0000

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+global _start
+
+section .text
+
+_start:
+	MOV rax, 1
+	MOV rdi, 1
+	MOV rsi, msg
+	MOV rdx, msglen
+	SYSCALL
+
+	MOV rax, 60
+	MOV rdi, 0
+	SYSCALL
+
+section .data
+	msg: DB "Hello, Holberton", 10
+	msglen: EQU $ - msg
