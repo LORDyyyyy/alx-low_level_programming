889c0ad5076556ff050dba378d8e482369d553b5
LORDyyyyy <mlord6271@gmail.com>
2023-04-27 16:56:48 +0000

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#incude "lists.h"
+
+/**
+ * free_list - t frees a list_t list
+ * @head: head
+ *
+ * Retrun: void
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (!head)
+		return;
+	while (head)
+	{
+		tmp = head;
+		free(tmp->str);
+		free(tmp);
+		head = head->next;
+	}
+	free(head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -33,4 +33,6 @@ list_t *add_node(list_t **head, const char *str);
 size_t _strlen(const char *s);
 
 list_t *add_node_end(list_t **head, const char *str);
+
+void free_list(list_t *head);
 #endif
