5e3a45d2cdf9307b9a88948b9127bb3e6d600315
LORDyyyyy <mlord6271@gmail.com>
2023-06-15 23:17:22 +0300

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp = add_dnodeint(h, n);
 		return (tmp);
 	}
-	
+
 	if (hh)
 		while (hh->prev)
 			hh = hh->prev;
